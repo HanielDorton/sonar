7967dc6e19ca0c9e9c227ba24c101bca5c0a5f3e
unknown <danhorton0@gmail.com>
2014-11-15 09:02:27 -0800

    Init Commit

---
+++ android/src/com/haniel/sonar/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.haniel.sonar.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.haniel.sonar.sonar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new sonar(), config);
+	}
+}
---
+++ desktop/src/com/haniel/sonar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.haniel.sonar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.haniel.sonar.sonar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new sonar(), config);
+	}
+}
